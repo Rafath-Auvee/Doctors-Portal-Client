5fcc9ace9e7294b4433960f0d75c4b45a48842ed
Rafath-Auvee <rbza98@outlook.com>
2023-04-12 03:05:28 +0600

    appointment fixed

--- src/Pages/Appointment/AppointmentCard/AppointmentCard.js
+++ src/Pages/Appointment/AppointmentCard/AppointmentCard.js
@@ -4,39 +4,47 @@ import ModalAppointment from "../ModalAppointment/ModalAppointment.js";
 import { format } from "date-fns";
 import Loading from "../../Shared/Loading/Loading.js"
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-// import ModalAppointment from "../ModalAppointment/ModalAppointment";
-
 const AppointmentCard = ({ date }) => {
   // const [services, setServices] = useState([]);
-  const [treatment, setTreatment] = useState(null)
-  const formattedDate = format(date, 'PP');
+  const [treatment, setTreatment] = useState(null);
+  const formattedDate = format(date, "PP");
+  const queryClient = new QueryClient()
 
-  const { isLoading, data: services, refetch } = useQuery(['available', formattedDate], () =>
-      fetch(`https://doctors-portal-server-sage-psi.vercel.app/available?date=${formattedDate}`)
-          .then(res => res.json())
-  )
-  if (isLoading) {
-      return <Loading></Loading>
+  const { data: services, isLoading, refetch } = useQuery(["available", formattedDate], () =>
+    fetch(`https://doctors-portal-server-sage-psi.vercel.app/available?date=${formattedDate}`).then((res) =>
+      res.json()
+    )
+  );
+
+  if(isLoading)
+  {
+    return <Loading/>
   }
+
+
   return (
-      <div>
-          <h4 className='text-xl text-secondary text-center mt-32 mb-9'>Appointments Available on {format(date, 'PP')}</h4>
-          <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 justify-items-center'>
-              {
-                  services?.map(service => <Service
-                      key={service._id}
-                      service={service}
-                      setTreatment={setTreatment}
-                  ></Service>)
-              }
-          </div>
-          {treatment && <ModalAppointment
-              setTreatment={setTreatment}
-              date={date}
-              treatment={treatment}
-              refetch={refetch}
-          ></ModalAppointment>}
+    <div>
+      <p className="text-center text-xl text-secondary text-bold">
+        Available Appointment on {format(date, "PP")}.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
+        {services.map((service) => (
+          <Services
+            key={service._id}
+            service={service}
+            setTreatment={setTreatment}
+          />
+        ))}
       </div>
+      {treatment && (
+        <ModalAppointment
+          date={date}
+          treatment={treatment}
+          setTreatment={setTreatment}
+          refetch={refetch}
+        />
+      )}
+    </div>
   );
 };
 
